982430f9b8f6ba7225da168ee66f1c22d543b44e
unknown <cosdeluz@gmail.com>
2023-05-01 20:25:09 +0200

    primer comit

---
+++ variables/enteras.go
@@ -0,0 +1,14 @@
+package variables
+
+import "fmt"
+
+func MostrarEnteros() {
+	var intcomun int
+	intde32 := int32(10)
+	intde64 := int64(100)
+
+	fmt.Println("intcomun = ", intcomun)
+	fmt.Println("intde34 = ", intde32)
+	fmt.Println("intde64 = ", intde64)
+
+}
